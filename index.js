7160af1af53887dc87f4ae747c5cc4ed2494eadc
MathewsTanui <tanuimathews02@gmail.com>
2024-03-27 09:21:37 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+// index.js
+
+function superbowlWin(record) {
+    // Use the find() method to search for a win result
+    const win = record.find(game => game.result === "W");
+
+    // If a win is found, return the year of the win, otherwise return undefined
+    return win ? win.year : undefined;
+}
+
+
